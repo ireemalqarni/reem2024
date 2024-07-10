27eb831d00d3e708c3c7467ddd7ee199c10dd9b1
reem <ireemalqarni@gmail.com>
2024-07-10 12:26:50 +0400

    commit br reem before rebase

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build' ,'index.html'))
 })
 
-// new commnent brach2 by ammar
+// new commnent brach2 by reem before rebase
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
