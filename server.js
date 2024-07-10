8a744b2cfd28302f01cf2ae5b29e52bad53afbac
ireemalqarni <132186207+ireemalqarni@users.noreply.github.com>
2024-07-10 13:07:33 +0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build' ,'index.html'))
 })
 
-
+//i'm a comment! , you should pull me
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
