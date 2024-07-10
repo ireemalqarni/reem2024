0d97e0ae70df67a7cc2a059423265c20c783bdfb
ireemalqarni <132186207+ireemalqarni@users.noreply.github.com>
2024-07-10 13:09:30 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.static(path.join(__dirname,'build')));
-// new comment
+// new comment !!!
 app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build' ,'index.html'))
 })
