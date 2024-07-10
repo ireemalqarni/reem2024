c6f6c9ae0e6690f8ce893e3cd04d22fe3a7c8257
reem <ireemalqarni@gmail.com>
2024-07-10 11:26:43 +0400

    first commit branch2 created2024

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname,'build')));
 app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build' ,'index.html'))
 })
-
+//
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
